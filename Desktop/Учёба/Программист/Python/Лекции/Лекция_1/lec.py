d27e014e683789ff26cd5867656639d7689cd11f
Ника Воронина <nika_voronina_95@mail.ru>
2022-09-28 14:55:09 +0300

    Сохранение лекции

---
+++ Desktop/Учёба/Программист/Python/Лекции/Лекция_1/lec.py
@@ -0,0 +1,30 @@
+# print("hello, world")
+
+# типы переменных:
+# int, float, boolean, str, list, None
+value = None
+print(type(value))
+
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+value = 12334
+print(type(value))
+s = "hello 'world'"
+
+print(s) # вывод строки
+print(a, '-', b, '-', s)
+print('{1} - {2} - {0}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
+
+f = True
+print(f)
+list = ['1', '2', '3', 'hello']
+col = ['hello', 1, 2, 4, 5, True]
+print(list)
+print(col)
+
+# print() - отвечает за вывод данных
+# input() - отвечает за ввод данных
+
