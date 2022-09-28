f6d2da76211b830ca105060509a32e516efdf5fb
Ника Воронина <nika_voronina_95@mail.ru>
2022-09-28 15:09:47 +0300

    Сохранение лекции

--- Desktop/Учёба/Программист/Python/Лекции/Лекция_1/lec.py
+++ Desktop/Учёба/Программист/Python/Лекции/Лекция_1/lec.py
@@ -2,29 +2,54 @@
 
 # типы переменных:
 # int, float, boolean, str, list, None
-value = None
-print(type(value))
 
-a = 123
-b = 1.23
-print(type(a))
-print(type(b))
-value = 12334
-print(type(value))
-s = "hello 'world'"
 
-print(s) # вывод строки
-print(a, '-', b, '-', s)
-print('{1} - {2} - {0}'.format(a, b, s))
-print(f'{a} - {b} - {s}')
+# value = None
+# print(type(value))
 
-f = True
-print(f)
-list = ['1', '2', '3', 'hello']
-col = ['hello', 1, 2, 4, 5, True]
-print(list)
-print(col)
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+# value = 12334
+# print(type(value))
+# s = "hello 'world'"
+
+# print(s) # вывод строки
+
+# print(a, '-', b, '-', s)
+# print('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+# f = True
+# print(f)
+# list = ['1', '2', '3', 'hello']
+# col = ['hello', 1, 2, 4, 5, True]
+# print(list)
+# print(col)
 
 # print() - отвечает за вывод данных
 # input() - отвечает за ввод данных
 
+# print('Введите a: ')
+# a = int(input())
+# print('Введите b: ')
+# b = int(input())
+# print(a, '+', b, '=', a + b)
+# print('{} {}'.format(a, b))
+# print(f'{a} {b}')
+
+# print('Введите a: ')
+# a = float(input())
+# print('Введите b: ')
+# b = float(input())
+# print(a, '+', b, '=', a + b)
+
+# // - деление без остатка (целое число по итогу)
+# % - остаток от деления
+# ** - возведение в степень
+# round - округление по математическим правилам
+# a = 1.3
+# b = 3
+# c = round(a * b, 3) # округление до трёх чисел
+# print(c)
