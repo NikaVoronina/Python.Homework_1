f73fdc45c51fcdb794859daad8ab8aca4cae59ed
Ника Воронина <nika_voronina_95@mail.ru>
2022-09-28 17:24:42 +0300

    Сохранение семинара

--- Desktop/Учёба/Программист/Python/Семинары/Seminar_1/sem1.py
+++ Desktop/Учёба/Программист/Python/Семинары/Seminar_1/sem1.py
@@ -7,14 +7,18 @@
 # - 25, 5 -> да
 # - 8,9 -> нет
 
-a = int(input('Введите a: '))
-b = int(input('Введите b: '))
-if a * a == b:
-    print('Да')
-elif b * b == a:
-    print('Да')
-else:
-    print('Нет')
+
+
+# a = int(input('Введите a: '))
+# b = int(input('Введите b: '))
+# if a * a == b:
+#     print('Да')
+# elif b * b == a:
+#     print('Да')
+# else:
+#     print('Нет')
+
+
 
 # 2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
 
@@ -22,3 +26,11 @@ else:
 
 # - 1, 4, 8, 7, 5 -> 8
 # - 78, 55, 36, 90, 2 -> 90
+
+a = int(input('Введите a: '))
+b = int(input('Введите b: '))
+c = int(input('Введите c: '))
+d = int(input('Введите d: '))
+e = int(input('Введите e: '))
+
+print(max(a, b, c, d, e))
