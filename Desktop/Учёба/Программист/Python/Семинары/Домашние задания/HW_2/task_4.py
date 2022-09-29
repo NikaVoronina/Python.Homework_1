c588fba4cbe977e523628dac6f1da0f294af36d8
Ника Воронина <nika_voronina_95@mail.ru>
2022-09-29 21:18:47 +0300

    Task_4 added

---
+++ Desktop/Учёба/Программист/Python/Семинары/Домашние задания/HW_2/task_4.py
@@ -0,0 +1,28 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
+# Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+
+from random import randint
+
+def get_numbers(n):
+    return [randint(-n/2, n) for i in range(-n, n + 1)]
+
+def get_data_from_file(path):
+    data = open(path, 'r')
+    dlist = [int(line.strip()) for line in data]
+    data.close()
+    return dlist
+
+def get_mult(numbers, datalist):
+    mult = 1
+    for i in datalist:
+        mult *= numbers[i]
+    return mult
+
+path = '17_Mult_task.txt'
+n = 10 
+datalist = get_data_from_file(path)
+numbers = get_numbers(n)
+
+print('Список:', numbers)
+print('Произведение элементов =', get_mult(numbers, datalist))
